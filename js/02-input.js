5a10b42c082c96f475deeafa0cfcdf6aee040494
grypis <grypisnecros@gmail.com>
2024-12-21 14:16:57 +0200

    asdas

--- js/02-input.js
+++ js/02-input.js
@@ -1,2 +1,23 @@
-const inputElem = document.querySelector('.js-user-name');
-console.log(inputElem);
+//!   ================================================
+const usernameElem = document.querySelector('.js-user-name');
+const btnElem = document.querySelector('.js-btn');
+
+usernameElem.addEventListener('input', onUsernameInput);
+
+function onUsernameInput() {
+  const value = usernameElem.value;
+  const len = usernameElem.dataset.len;
+
+  if (value.length > len) {
+    usernameElem.classList.add('valid');
+    usernameElem.classList.remove('invalid');
+    btnElem.disabled = false;
+  } else {
+    usernameElem.classList.add('invalid');
+    usernameElem.classList.remove('valid');
+    btnElem.disabled = true;
+  }
+  console.log(value);
+}
+
+//!   ================================================
